2a894b575ca8210ad6e815e519f9a671e163f9e0
devabormon <dev.abormon28@gmail.com>
2023-03-20 00:10:00 +0600

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
+const Sidebar = () => {
+    const items = [
+        {
+            label: 'Home',
+            href: '/',
+            icon: BsHouseFill,
+        },
+        {
+            label: 'Notifications',
+            href: '/notificaitons',
+            icon: BsBellFill,
+        },
+        {
+            label: 'Profile',
+            href: '/users/123',
+            icon: FaUser,
+        },
+    ];
+    return (
+        <div className="col-span-1 h-full pr-4 md:pr-6">
+            <div className="flex flex-col items-end">
+                <div className="space-y-2 lg:w-[230px]">as</div>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
